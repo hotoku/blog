f11fae63248c5ca8624b044be8b7189287ba2020
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-06-28 12:28:11 +0900

    plist2

--- _plist2/run.sh
+++ _plist2/run.sh
@@ -2,11 +2,17 @@
 
 
 echo $(date) $*
+echo ${PATH}
 
 
 print_usage(){
     echo "run.sh"
 }
+
+
+export PATH=/usr/local/bin:${PATH}
+source $(brew --prefix)/opt/chruby/share/chruby/chruby.sh
+source $(brew --prefix)/opt/chruby/share/chruby/auto.sh
 
 
 jekyll serve --drafts --livereload
